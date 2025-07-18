6c248876abfeae70acca0549e05f88ce0924393a
Manavarya09 <smanavarya@gmail.com>
2025-07-18 11:57:40 +0400

    feat: add sleek golden scroll progress bar at top of page

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import About from "./pages/About";
+import ScrollProgressBar from "./components/ScrollProgressBar";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
+      <ScrollProgressBar />
       <Toaster />
       <Sonner />
       <BrowserRouter>
